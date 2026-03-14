5d5e786342ce63e91a8b4d4fefdafea943b52fc9
Ryan <mukalazironnie@live.com>
2026-03-14 20:12:22 +0300

    Modify config.js with new environment variable values
    Updated configuration values for MODE, SESSION_ID, AUTO_READ_STATUS, and AUTO_LIKE_STATUS.

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ if (fs.existsSync(".env"))
     require("dotenv").config({ path: __dirname + "/.env", quiet: true });
 
 module.exports = {
-    MODE: process.env.MODE,
-    SESSION_ID: process.env.SESSION_ID,
+    MODE: process.env.MODE,'private' 
+    SESSION_ID: process.env.SESSION_ID,'Gifted~HBek6ZJWYoUH' 
     TIME_ZONE: process.env.TIME_ZONE,
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS,
-    AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS,
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS, 'true' 
+    AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS, 'true' 
     DATABASE_URL: process.env.DATABASE_URL, // Postress(Get one for free from neon.tech/supabase/render/heroku builtin postgress)...will fallback to path: ./gift/database/database.db if not provided
 };
 
